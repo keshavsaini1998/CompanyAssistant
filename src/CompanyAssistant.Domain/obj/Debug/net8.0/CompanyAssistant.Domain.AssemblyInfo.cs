76db38252b89dec993b639b1d33eafb82eab9f54
Keshav Saini <keshav.saini@zenegy.dev>
2026-01-23 05:35:15 +0530

    chore: remove IDE and build artifacts from repository

--- src/CompanyAssistant.Domain/obj/Debug/net8.0/CompanyAssistant.Domain.AssemblyInfo.cs
+++ src/CompanyAssistant.Domain/obj/Debug/net8.0/CompanyAssistant.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyAssistant.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9375bab2fc5b9ccf9b36b1bb5fca67b1a3f181a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyAssistant.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyAssistant.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
